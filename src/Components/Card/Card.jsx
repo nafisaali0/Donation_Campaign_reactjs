ab707f92fcc95693364549ad0b7d7369908a8263
nafisaali0 <nafisaali20006@gmail.com>
2024-07-17 00:30:08 +0600

    edit card ui

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,20 +3,28 @@ import { Link } from 'react-router-dom';
 
 
 const Card = ({ card }) => {
-    
+
     const { id, category_name, title, card_image, category_text, title_color, category_bg } = card
 
     return (
         <>
             <Link to={`/carddetails/${id}`}>
-                <div className="card bg-base-100 shadow-xl" style={{ background: category_bg }} >
-                    <figure><img className='w-full' src={card_image} alt="Shoes" /></figure>
-                    <div className="card-body">
-                        <button className="text-center font-semibold w-24 px-3 py-2 rounded-md" style={{ color: category_text, background: category_bg }}>{category_name}</button>
-                        <p className='font-bold' style={{ color: title_color }}>{title}</p>
+                <div className="card shadow-xl flex flex-col h-full" style={{ background: category_bg }}>
+                    <figure className="w-full h-64 overflow-hidden">
+                        <img
+                            className="w-full h-full object-cover"
+                            src={card_image}
+                            alt="category_image"
+                        />
+                    </figure>
+                    <div className="card-body flex flex-col flex-grow">
+                        <h2 className="card-title" style={{ color: title_color }}>{title}</h2>
+                        <div className="mt-auto justify-end">
+                            <button className="btn border-none" style={{ color: category_text, background: category_bg }}>{category_name}</button>
+                        </div>
                     </div>
                 </div>
-            </Link >
+            </Link>
         </>
     );
 };
